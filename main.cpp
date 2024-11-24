b702417830b4a995f074dce2bae0f6c6a9543d11
seunghyeokleeme <seunghyeok.tech@gmail.com>
2024-11-24 18:48:44 +0900

    feat: 동적 할당

--- main.cpp
+++ main.cpp
@@ -1,56 +1,42 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-const int kMaxStr = 100; // 전역 상수 소개
+const int kMaxStr = 100; // 전역 상수
 
-// 문자열을 매개변수로 넣기
-// 여기서는 모든 문자열 배열의 길이가 동일하다고 가정
-bool IsEqual(const char str1[], const char str2[])
+int Min(int a, int b)
 {
-    // 크기 출력 확인 (배열 크기가 아님 주의!) - 문자열의 길이를 별도로 저장해야 합니다!
-    // cout << sizeof(str1) << " " << sizeof(str2) << " " << endl;
-    // exit(-1);
-
-    // 힌트: ==, != 같지 않다 비교 연산자
-    // 힌트: 문자열 종료 조건
-    // 디버깅 힌트: 문자를 정수로 바꿔서 출력해보기
-
-    for (int i = 0; i < kMaxStr; i++)
-    {
-        cout << int(str1[i]) << " " << int(str2[i]) << endl;
-
-        if (str1[i] != str2[i])
-            return false;
-
-        if (str1[i] == '\0')
-            return true;
-    }
-
-    return true;
+    return a < b ? a : b; // 조건연산자(삼항연산자)
 }
 
 int main()
 {
-    // 영어 사용이 디버깅에 유리합니다.
-    const char str1[kMaxStr] = "stop";
+    // 문자열 복사
+    char str1[] = "Hello, World!";
+    char str2[kMaxStr];
 
-    while (1)
-    {
-        // TODO:
-        char str2[kMaxStr];
-        cin >> str2;
+    // dest, src 안내 (복사할 메모리 크기 주의)
+    memcpy(str2, str1, Min(sizeof(str1), sizeof(str2)));
+    cout << str2 << endl;
 
-        if (IsEqual(str1, str2))
-        {
-            cout << "종료합니다." << endl;
-            break;
-        }
-        else
-        {
-            cout << "계속합니다." << endl;
-        }
-    }
+    char *dynamic_array = new char[kMaxStr];
+
+    // 주의: 동적할당 메모리는 변수 사이즈가 포인터 사이즈임 (배열이 아님)
+    memcpy(dynamic_array, str1, Min(sizeof(str1), sizeof(kMaxStr)));
+    // memcpy(dynamic_array, str2, kMaxStr);
+    cout << dynamic_array << endl;
+
+    cout << str1 << " " << str2 << " " << dynamic_array << endl;
+    cout << size_t(str1) << " " << size_t(str2) << " " << size_t(dynamic_array) << endl;
+
+    // 보통 크기를 별도로 저장함
+
+    delete[] dynamic_array; // 배열 삭제시 []
+
+    // 지우지 않고 재할당할 경우 잃어버림
+    // dynamic_array = new char[원하는크기];
+    // delete[] dynamic_array; 다시 지워줘야 함
 
     return 0;
 }
