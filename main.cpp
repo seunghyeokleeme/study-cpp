e4925d7a0f9b3a2c8144b694178bd25bbfdffd3b
seunghyeokleeme <seunghyeok.tech@gmail.com>
2024-11-24 14:39:03 +0900

    feat: 흐름 제어

--- main.cpp
+++ main.cpp
@@ -1,49 +1,46 @@
-/*
-    홍정모 연구소 https://honglab.co.kr/
-*/
-
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    // ...
+    /* for (int i = 5; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl; */
 
-    // 같은 자료형의 데이터를 저장하기 위해 메모리를 미리 잡아놓은 것
-    int my_array[3] = {1, 2, 3}; // 초기화할 때는 {} 안에 값 나열
+    /* int my_array[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
 
-    // 인덱싱 (zero-based)
-    cout << my_array[0] << " "
-         << my_array[1] << " "
-         << my_array[2] << endl; // 추측해보세요
+    for (int i = 0; i < sizeof(my_array) / sizeof(int); i++)
+    {
+        if (i > 0)
+        {
+            if (my_array[i] < my_array[i - 1])
+                break;
+        }
+        cout << my_array[i] << " ";
+    }
+    cout << endl; */
 
-    // 인덱싱으로 하나 짜리 변수 처럼 사용 가능
-    my_array[1] = 5;
+    /*     char my_string[] = "Hello, World!";
 
-    cout << my_array[0] << " "
-         << my_array[1] << " "
-         << my_array[2] << endl; // 추측해보세요
+        for (int i = 0; my_string[i] != '\0'; i++)
+        {
+            cout << i << " " << my_string[i] << endl;
+        }
+        cout << endl; */
 
-    // cout << my_array[10000] << endl;
+    char my_string[] = "Hello, World!";
 
-    // 문자열은 기본적으로 문자의 배열
-    char name[] = "Hello, World!"; // 문자''와 문자열"" 구분
-    // Null character '\0'
-    cout << name << " " << sizeof(name) << endl; // 추측해보세요
+    int i = 0;
 
-    name[0] = 'A';
-    name[1] = 'B';
-    name[2] = 'C';
-
-    cout << name << endl; // 추측해보세요
-
-    name[2] = '\0'; // 추측해보세요
-
-    cout << name << endl; // 추측해보세요
+    while (my_string[i] != '\0')
+    {
+        cout << my_string[i];
+        i++;
+    }
+    cout << endl;
 
     return 0;
 }
