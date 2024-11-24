53ab07ca4b6bf9f14041a2420612145efcb2385b
seunghyeokleeme <seunghyeok.tech@gmail.com>
2024-11-24 18:36:18 +0900

    feat: 문자열 비교

--- main.cpp
+++ main.cpp
@@ -2,28 +2,55 @@
 
 using namespace std;
 
-int Add(int a, int b);
+const int kMaxStr = 100; // 전역 상수 소개
 
-void Add(int a, int b, int *c, int *d)
+// 문자열을 매개변수로 넣기
+// 여기서는 모든 문자열 배열의 길이가 동일하다고 가정
+bool IsEqual(const char str1[], const char str2[])
 {
-    *c = a + b;
-    *d = a - b;
+    // 크기 출력 확인 (배열 크기가 아님 주의!) - 문자열의 길이를 별도로 저장해야 합니다!
+    // cout << sizeof(str1) << " " << sizeof(str2) << " " << endl;
+    // exit(-1);
+
+    // 힌트: ==, != 같지 않다 비교 연산자
+    // 힌트: 문자열 종료 조건
+    // 디버깅 힌트: 문자를 정수로 바꿔서 출력해보기
+
+    for (int i = 0; i < kMaxStr; i++)
+    {
+        cout << int(str1[i]) << " " << int(str2[i]) << endl;
+
+        if (str1[i] != str2[i])
+            return false;
+
+        if (str1[i] == '\0')
+            return true;
+    }
+
+    return true;
 }
 
 int main()
 {
-    cout << Add(1, 2) << endl;
+    // 영어 사용이 디버깅에 유리합니다.
+    const char str1[kMaxStr] = "stop";
 
-    int sum;
-    int sub;
-    Add(4, 5, &sum, &sub);
+    while (1)
+    {
+        // TODO:
+        char str2[kMaxStr];
+        cin >> str2;
 
-    cout << sum << " " << sub << endl;
+        if (IsEqual(str1, str2))
+        {
+            cout << "종료합니다." << endl;
+            break;
+        }
+        else
+        {
+            cout << "계속합니다." << endl;
+        }
+    }
 
     return 0;
-}
-
-int Add(int a, int b)
-{
-    return a + b;
 }
